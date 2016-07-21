6faee54e230ae5a35b05c1de3cc768189a1c00be
ama <ama@d4949b6a-f376-7335-0820-ce57b7db7bff>
2016-07-21 18:48:20 +0000

    Merge branch 'master' of https://github.com/anthony-ma/meetuppres-react
    git-svn-id: https://github.com/mandevillemeetup/meetuppres-react.git/trunk@5 d4949b6a-f376-7335-0820-ce57b7db7bff

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,8 @@ var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
 var webpack = require('webpack');
 
-var srcPath = 'src';
-var libPath = 'lib';
+var srcPath = 'july-meetup';
+var libPath = 'july-dist';
 
 function getFolders(dir) {
     return fs.readdirSync(dir)
