8f4286ef648649d4a581c73cb6b9846aab5e2128
ashtonstockman <ashtonstockman@d4949b6a-f376-7335-0820-ce57b7db7bff>
2016-07-21 19:55:34 +0000

    more readme updates
    git-svn-id: https://github.com/mandevillemeetup/meetuppres-react.git/trunk@13 d4949b6a-f376-7335-0820-ce57b7db7bff

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,6 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var babel = require('gulp-babel');
-var debug = require('gulp-debug');
 var webpack = require('webpack');
 
 var srcPath = 'july-meetup';
@@ -34,17 +33,14 @@ gulp.task('clean', function (cb) {
 gulp.task('scripts', ['clean'], function() {
    var folders = getFolders(srcPath);
 
-console.log(folders);
    //foreach folder
    var tasks = folders.map(function(folder) {
     //foreach file in a folder
-      return gulp.src(path.join(srcPath, folder, '/**/*.jsx'))
+      return gulp.src(path.join(srcPath, folder, '/**/*.js'))
         // transpile
         .pipe(babel())
-        // log the file names for debug
-        .pipe(debug())
         // write to output
-        .pipe(gulp.dest(path.join(srcPath, folder)));
+        .pipe(gulp.dest(libPath));
    });
 
    return tasks;
